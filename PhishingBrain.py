07a7a8a2b916410f47400b692520a68c6621be4a
barhait9 <barhait10@gmail.com>
2025-03-10 15:12:11 +0000

    syntax

--- PhishingBrain.py
+++ PhishingBrain.py
@@ -1,11 +1,12 @@
 import pandas as pd
 import numpy as np
+import tensorflow as tf
+from tensorflow.keras import layers
 
 # Make numpy values easier to read.
 np.set_printoptions(precision=3, suppress=True)
 
-import tensorflow as tf
-from tensorflow.keras import layers
+tf.keras.preprocessing.sequence.skipgram()
 
 
 
