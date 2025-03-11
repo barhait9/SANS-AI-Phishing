00841c71e93af0f866412cbd0dfb9349bdbff52b
H4SSA4AN <hassan_touchent@outlook.com>
2025-03-11 16:10:45 +0000

    Created a data formatter file

---
+++ DataFormatter.py
@@ -0,0 +1,5 @@
+'''
+This file will separate the CSV data into 3 arrays: subject, body, label
+subject[0], body[0], label[0] will correspond to the first email
+This file will then have all the data for the Word2Vec to train from
+'''
