a6f5410af21ce7ab4e0154608bef98a899ffeb73
barhait9 <barhait10@gmail.com>
2025-03-11 16:05:08 +0000

    changed file name had to do it the long way

--- Word2Vec.py
+++ Word2Vec.py
@@ -7,9 +7,9 @@ from tensorflow.keras import layers
 np.set_printoptions(precision=3, suppress=True)
 
 tf.keras.preprocessing.sequence.skipgrams()
-
-
-
-
+epochs = 50
+learning_rate = 0.01
+batch_size = 16
+embedding_size = 300  #might change to 500 if it makes it better
 
 
